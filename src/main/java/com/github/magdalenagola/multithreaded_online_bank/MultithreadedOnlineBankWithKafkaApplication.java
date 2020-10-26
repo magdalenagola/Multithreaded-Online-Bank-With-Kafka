8715e03bf95e290629c8371cd26d81247b228ca0
Magdalena Gola <turcza.magdalena@gmail.com>
2020-10-26 17:03:11 +0100

    First commit

---
+++ src/main/java/com/github/magdalenagola/multithreaded_online_bank/MultithreadedOnlineBankWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.github.magdalenagola.multithreaded_online_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadedOnlineBankWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadedOnlineBankWithKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/magdalenagola/multithreaded_online_bank/MultithreadedOnlineBankWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.magdalenagola.multithreaded_online_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadedOnlineBankWithKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
