d6445653d33aae67333d3ebef4f3283e2c621469
szczepanTopolski <szczepanPL678@gmail.com>
2020-10-29 13:39:09 +0100

    Adjust producer parameters to dto introduction

--- src/main/java/com/github/magdalenagola/multithreaded_online_bank/transaction/Producer.java
+++ src/main/java/com/github/magdalenagola/multithreaded_online_bank/transaction/Producer.java
@@ -1,6 +1,6 @@
 package com.github.magdalenagola.multithreaded_online_bank.transaction;
 
-import com.github.magdalenagola.multithreaded_online_bank.model.Transaction;
+import com.github.magdalenagola.multithreaded_online_bank.model.TransactionDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -11,20 +11,20 @@ import org.springframework.util.concurrent.ListenableFutureCallback;
 public class Producer implements Runnable {
     private static final Logger LOG = LoggerFactory.getLogger(Producer.class);
     private final String TOPIC = "transaction";
-    private final KafkaTemplate<String,Transaction> kafkaTemplate;
-    private final Transaction transaction;
+    private final KafkaTemplate<String,TransactionDTO> kafkaTemplate;
+    private final TransactionDTO transaction;
 
-    public Producer(Transaction transaction, KafkaTemplate<String, Transaction> kafkaTemplate) {
+    public Producer(TransactionDTO transaction, KafkaTemplate<String, TransactionDTO> kafkaTemplate) {
         this.transaction = transaction;
         this.kafkaTemplate = kafkaTemplate;
     }
 
     @Override
     public void run()throws IllegalArgumentException {
-        ListenableFuture<SendResult<String, Transaction>> future = kafkaTemplate.send(TOPIC, transaction);
-        future.addCallback(new ListenableFutureCallback<SendResult<String, Transaction>>() {
+        ListenableFuture<SendResult<String, TransactionDTO>> future = kafkaTemplate.send(TOPIC, transaction);
+        future.addCallback(new ListenableFutureCallback<SendResult<String, TransactionDTO>>() {
             @Override
-            public void onSuccess(SendResult<String, Transaction> result) {
+            public void onSuccess(SendResult<String, TransactionDTO> result) {
                 synchronized (Producer.class) {
                     LOG.info("Transaction [{}] delivered with offset {}",
                             transaction.toString(),
