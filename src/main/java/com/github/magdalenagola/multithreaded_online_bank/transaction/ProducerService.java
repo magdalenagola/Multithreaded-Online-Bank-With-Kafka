28827c550af72aace74b9950c5ac452bf42199bc
szczepanTopolski <szczepanPL678@gmail.com>
2020-10-29 13:39:34 +0100

    Adjust producer service to dto introduction

--- src/main/java/com/github/magdalenagola/multithreaded_online_bank/transaction/ProducerService.java
+++ src/main/java/com/github/magdalenagola/multithreaded_online_bank/transaction/ProducerService.java
@@ -1,6 +1,6 @@
 package com.github.magdalenagola.multithreaded_online_bank.transaction;
 
-import com.github.magdalenagola.multithreaded_online_bank.model.Transaction;
+import com.github.magdalenagola.multithreaded_online_bank.model.TransactionDTO;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +12,13 @@ import java.util.concurrent.Future;
 @Service
 public class ProducerService {
     private static final ExecutorService executorService = Executors.newFixedThreadPool(10);
-    private final KafkaTemplate<String, Transaction> kafkaTemplate;
+    private final KafkaTemplate<String, TransactionDTO> kafkaTemplate;
 
-    public ProducerService(KafkaTemplate<String, Transaction> kafkaTemplate) {
+    public ProducerService(KafkaTemplate<String, TransactionDTO> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    void sendToKafka(Transaction transaction)throws IllegalArgumentException{
+    public void sendToKafka(TransactionDTO transaction)throws IllegalArgumentException{
         Runnable producer = new Producer(transaction, kafkaTemplate);
         Future<?> future = executorService.submit(producer);
         try {
@@ -26,8 +26,8 @@ public class ProducerService {
               if(!future.isDone())
                 throw new IllegalArgumentException("Unable to send to kafka");
         } catch (InterruptedException | ExecutionException e) {
-           throw new IllegalArgumentException("Unable to send to kafka");
+           //throw new IllegalArgumentException("Unable to send to kafka");
+            e.printStackTrace();
         }
-
     }
 }
