ee1d0b4aab169eb950e5425a8290cb2d77ea4a7f
szczepanTopolski <szczepanPL678@gmail.com>
2020-11-02 13:34:21 +0100

    Add account number validator

---
+++ src/main/java/com/github/magdalenagola/multithreaded_online_bank/validator/AccountNumberValidator.java
@@ -0,0 +1,15 @@
+package com.github.magdalenagola.multithreaded_online_bank.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AccountNumberValidator implements ConstraintValidator<AccountNumber,String> {
+    @Override
+    public boolean isValid(String accountNumber, ConstraintValidatorContext constraintValidatorContext) {
+        Pattern pattern = Pattern.compile("^([A-Z]{2}[ \\-]?[0-9]{2})(?=(?:[ \\-]?[A-Z0-9]){9,30}$)((?:[ \\-]?[A-Z0-9]{3,5}){2,7})([ \\-]?[A-Z0-9]{1,3})?$");
+        Matcher matcher = pattern.matcher(accountNumber);
+        return matcher.matches();
+    }
+}
