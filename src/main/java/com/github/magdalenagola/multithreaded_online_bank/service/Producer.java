76982ef914e23182812891659cf5a15ca0089611
szczepanTopolski <szczepanPL678@gmail.com>
2020-11-04 13:34:05 +0100

    Change kafka tempalte to replyingKafkaTemplate, runnable to callable, returns result

--- src/main/java/com/github/magdalenagola/multithreaded_online_bank/service/Producer.java
+++ src/main/java/com/github/magdalenagola/multithreaded_online_bank/service/Producer.java
@@ -1,30 +1,37 @@
 package com.github.magdalenagola.multithreaded_online_bank.service;
 
+import com.github.magdalenagola.multithreaded_online_bank.model.Reply;
 import com.github.magdalenagola.multithreaded_online_bank.model.TransactionDTO;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
+import org.springframework.kafka.requestreply.RequestReplyFuture;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 
-public class Producer implements Runnable {
+public class Producer implements Callable<Reply> {
     private static final Logger LOG = LoggerFactory.getLogger(Producer.class);
     private final String TOPIC = "transaction";
-    private final KafkaTemplate<String,TransactionDTO> kafkaTemplate;
+    private final ReplyingKafkaTemplate<String,TransactionDTO, Reply> kafkaTemplate;
     private final TransactionDTO transaction;
 
-    public Producer(TransactionDTO transaction, KafkaTemplate<String, TransactionDTO> kafkaTemplate) {
+    public Producer(TransactionDTO transaction, ReplyingKafkaTemplate<String, TransactionDTO,Reply> kafkaTemplate) {
         this.transaction = transaction;
         this.kafkaTemplate = kafkaTemplate;
     }
 
     @Override
-    public void run() {
-        ListenableFuture<SendResult<String, TransactionDTO>> future = kafkaTemplate.send(TOPIC, transaction);
+    public Reply call() {
+        ProducerRecord<String,TransactionDTO> producerRecord = new ProducerRecord<>(TOPIC,transaction);
+        RequestReplyFuture<String,TransactionDTO, Reply> replyFuture = kafkaTemplate.sendAndReceive(producerRecord);
+        ListenableFuture<SendResult<String, TransactionDTO>> future = replyFuture.getSendFuture();
         future.addCallback(new ListenableFutureCallback<SendResult<String, TransactionDTO>>() {
             @Override
             public void onSuccess(SendResult<String, TransactionDTO> result) {
@@ -45,7 +52,8 @@ public class Producer implements Runnable {
         });
 
         try {
-            future.get();
+            ConsumerRecord<String, Reply> stringReplyConsumerRecord = replyFuture.get();
+            return stringReplyConsumerRecord.value();
         } catch (InterruptedException | ExecutionException | CancellationException e) {
             throw new RuntimeException("Future failure");
         }
